59deeed508365e9c33c430e9309d857cb448cf9b
sabinar <sabina.ravindran@gmail.com>
2014-11-09 13:18:35 -0800

    temp commit

--- src/main/java/com/example/model/Device.java
+++ src/main/java/com/example/model/Device.java
@@ -36,9 +36,9 @@ public class Device {
 	private Person person;
 
 	//@ManyToMany ( mappedBy = "devices")
-	//@ManyToMany (cascade = {CascadeType.PERSIST, CascadeType.MERGE }, fetch = FetchType.EAGER)
+	//@ManyToMany (cascade = {CascadeType.PERSIST, CascadeType.MERGE }, fetch = FetchType.EAGER) , mappedBy = "devices"
     //@JoinTable(name = "device_application", joinColumns = { @JoinColumn(name = "deviceId") }, inverseJoinColumns = { @JoinColumn(name = "appId") })
-	@ManyToMany (cascade = {CascadeType.PERSIST, CascadeType.MERGE }, mappedBy = "devices")
+	@ManyToMany (cascade = {CascadeType.PERSIST, CascadeType.MERGE })
 	@JoinTable(name = "device_application", joinColumns = { @JoinColumn(name = "deviceId") }, inverseJoinColumns = { @JoinColumn(name = "appId") })
     private List<Application> applications = new ArrayList<Application>();
 	
