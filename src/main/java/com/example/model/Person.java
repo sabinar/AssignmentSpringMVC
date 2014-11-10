5da7b1b1ec556b0a4d5d4f7f7d590d241601854d
sabinar <sabina.ravindran@gmail.com>
2014-11-09 18:29:00 -0800

    removing all changes

--- src/main/java/com/example/model/Person.java
+++ src/main/java/com/example/model/Person.java
@@ -14,7 +14,6 @@ import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
-@Table(name="Person",schema="ExampleSchema")
 public class Person {
 
     @Id
